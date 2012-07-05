f516a010ffbe3829614fffb76209679215a19fb3
Olivier Favre <olivier@yakaz.com>
2012-07-05 18:24:23 +0200

    Extract real parameter types
    No longer output a "string representation" of the default value,
    rather further parse the values to extract raw ruby values,
    that finally get experted as native JSON values.

--- lib/proxy/puppet/puppet_class.rb
+++ lib/proxy/puppet/puppet_class.rb
@@ -27,8 +27,7 @@ module Proxy::Puppet
             params = {}
             # Get parameters and eventual default values
             klass.arguments.each do |name, value|
-              value = value.to_s unless value == nil
-              params[name] = value
+              params[name] = ast_to_value(value) rescue nil
             end
             klasses << new(klass.namespace, params)
           end
@@ -67,6 +66,82 @@ module Proxy::Puppet
 
     def has_module?(klass)
       !!klass.index("::")
+    end
+
+    def self.ast_to_value value
+      unless value.class.name.start_with? "Puppet::Parser::AST::"
+        # Native Ruby types
+        case value
+        # Supported with exact JSON equivalent
+        when NilClass, String, Numeric, Array, Hash, FalseClass, TrueClass
+          value
+        when Struct
+          value.hash
+        when Enumerable
+          value.to_a
+        # Stringified
+        when Regexp # /(?:stringified)/
+          "/#{value.to_s}/"
+        when Symbol # stringified
+          value.to_s
+        else
+          raise TypeError
+        end
+      else
+        # Parser types
+        case value
+        # Supported with exact JSON equivalent
+        when Puppet::Parser::AST::Boolean, Puppet::Parser::AST::String
+          value.evaluate nil
+        # Supported with stringification
+        when Puppet::Parser::AST::Concat
+          # Note1: only simple content are supported, plus variables whose raw name is taken
+          # Note2: The variable substitution WON'T be done by Puppet from the ENC YAML output
+          value.value.map { |v|
+            case v
+            when Puppet::Parser::AST::String
+              v.evaluate nil
+            when Puppet::Parser::AST::Variable
+              "${#{v.value}}"
+            else
+              raise TypeError
+            end
+          }.join rescue nil
+        when Puppet::Parser::AST::Type
+          value.value
+        when Puppet::Parser::AST::Name
+          (Puppet::Parser::Scope.number?(value.value) or value.value)
+        when Puppet::Parser::AST::Undef # equivalent of nil
+          nil
+        # Depends on content
+        when Puppet::Parser::AST::ASTArray
+          value.inject([]) { |arr, v| (arr << ast_to_value(v)) rescue arr }
+        when Puppet::Parser::AST::ASTHash
+          Hash[value.value.each.inject([]) { |arr, (k,v)| (arr << [ast_to_value(k), ast_to_value(v)]) rescue arr }]
+        # Let's see if a raw evaluation works with no scope for any other type
+        else
+          if value.respond_to? :evaluate
+            # Can probably work for: (depending on the actual content)
+            # - Puppet::Parser::AST::ArithmeticOperator
+            # - Puppet::Parser::AST::ComparisonOperator
+            # - Puppet::Parser::AST::BooleanOperator
+            # - Puppet::Parser::AST::Minus
+            # - Puppet::Parser::AST::Not
+            # May work for:
+            # - Puppet::Parser::AST::InOperator
+            # - Puppet::Parser::AST::MatchOperator
+            # - Puppet::Parser::AST::Selector
+            # Probably won't work for
+            # - Puppet::Parser::AST::Variable
+            # - Puppet::Parser::AST::HashOrArrayAccess
+            # - Puppet::Parser::AST::ResourceReference
+            # - Puppet::Parser::AST::Function
+            value.evaluate nil
+          else
+            raise TypeError
+          end
+        end
+      end
     end
 
   end
